a4341b7d4874a2d004a359370b2890e2c1c73c2c
Dominick DeCanio <domdecanio@gmail.com>
2022-07-11 14:41:08 -0400

    updates

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Test Package 1',
+    name='testpackage1',
     version='1.0.0',
-    url='https://github.com/domdecanio/Test-Package-1',
+    url='https://github.com/domdecanio/testpackage1',
     author='Dominick DeCanio',
     description='This package is created as a test. It contains 1 function which performs addition',
-    packages=['test_package_1_module']
+    packages=['testpackage1']
     install_requires=[],
 )
