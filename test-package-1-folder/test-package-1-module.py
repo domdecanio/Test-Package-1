ddf07133cc8f024f144f02570f997ff0d0851477
Dominick DeCanio <domdecanio@gmail.com>
2022-07-11 13:14:29 -0400

    Rest of Package files

---
+++ test-package-1-folder/test-package-1-module.py
@@ -0,0 +1,9 @@
+def test_package_1_funct(x):
+    """
+    Purpose - This function adds 1 to the inputted integer, and prints the result.
+    
+    Inputs: x - integer
+    Output: x + 1
+    """
+    y = x + 1
+    print(y)
